baf484adf8ca8cb63cf84a4627b893d4508c7786
nourhene <soueidnourhene@gmail.com>
2021-04-26 12:09:40 +0200

    Move startup initialization code to Scene delegate

--- Projet/AppDelegate.swift
+++ Projet/AppDelegate.swift
@@ -1,27 +1,9 @@
 import UIKit
-import Amplify
-import AmplifyPlugins
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    func configureAmplify() {
-        Amplify.Logging.logLevel = .info
-        let dataStorePlugin = AWSDataStorePlugin(modelRegistration: AmplifyModels())
-        let apiPlugin = AWSAPIPlugin(modelRegistration: AmplifyModels())
-        do {
-            try Amplify.add(plugin: dataStorePlugin)
-            try Amplify.add(plugin: apiPlugin)
-            try Amplify.configure()
-            print("Initialized Amplify");
-        } catch {
-            print("Could not initialize Amplify: \(error)")
-        }
-    }
-    
+        
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        configureAmplify()
-
         return true
     }
     
---
+++ Projet/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  Projet
+//
+//  Created by nourhene on 26/04/2021.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .gray
+    }
+    
+}
